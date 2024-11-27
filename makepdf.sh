df1a5a399557509239bdbd2ad1fdfe6550126e50
ReachableCEO <reachableceo@turnsys.com>
2024-11-27 07:19:44 -0600

    upping my pdf output game

--- makepdf.sh
+++ makepdf.sh
@@ -2,12 +2,16 @@
 
 INPUT_FILE="$1"
 OUTPUT_FILE="$(basename $INPUT_FILE).pdf"
-METADATA_FILE="~/bin/rcdoc/metadata/daily-stakeholder-report.yml"
+METADATA_FILE="$2"
+TEMPLATE="eisvogel"
+PANDOC_OPTIONS="--listings"
+
+# Setup proper boilerplate to handle cmd line options , check args etc...
 
 pandoc \
 $INPUT_FILE \
 -o $OUTPUT_FILE \
+--template $TEMPLATE \
+--metadata-file=$METADATA_FILE
 --from markdown \
---template eisvogel \
---listings \
---metadata-file=~$METADATA_FILE
+$PANDOC_OPTIONS
